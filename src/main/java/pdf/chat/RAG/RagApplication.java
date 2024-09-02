d56a253bd2c9ef5210b8b0ee36c0f9eacf6b8c8f
zark <77061323+zarkk01@users.noreply.github.com>
2024-09-02 10:13:06 +0300

    Update RagApplication.java

--- src/main/java/pdf/chat/RAG/RagApplication.java
+++ src/main/java/pdf/chat/RAG/RagApplication.java
@@ -1,13 +1,5 @@
 package pdf.chat.RAG;
 
-//                                                    ,--.
-//        ,---.  ,-|  /  .-' ,--.--. ,---.  ,--,--. ,-|  |,---. ,--.--. ,--.--.,--,--. ,---.
-//        | .-. |' .-. |  `-, |  .--'| .-. :' ,-.  |' .-. | .-. :|  .--' |  .--' ,-.  || .-. |
-//        | '-' '\ `-' |  .-' |  |   \   --.\ '-'  |\ `-' \   --.|  |    |  |  \ '-'  |' '-' '
-//        |  |-'  `---'`--'   `--'    `----' `--`--' `---' `----'`--'    `--'   `--`--'.`-  /
-//        `--'                                                                         `---'
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
