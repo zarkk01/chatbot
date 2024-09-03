786ebd5a360a93146b620a976917d209e18d474b
jaspass04 <t8220122@aueb.gr>
2024-09-03 15:43:45 +0300

    did i add the button fix?

--- src/main/resources/frontend/src/components/UploadButton.jsx
+++ src/main/resources/frontend/src/components/UploadButton.jsx
@@ -3,22 +3,17 @@ import './UploadButton.css';
 
 const UploadButton = () => {
     const handleFileUpload = async (e) => {
-    //     // const file = e.target.files[0];
-    //     // if (file) {
-    //         const formData = new FormData();
-    //         formData.append('file', file);
-
+            console.log("blabla")
             await fetch('http://localhost:8080/load', {
                 method: 'POST',
-                // body: formData,
             });
 
     };
 
     return (
         <div className="upload-button">
-            <input type="file" onChange={handleFileUpload} />
-            <button>Upload PDF</button>
+            <input type="file" onChange={handleFileUpload}/>
+            <button onClick={handleFileUpload}>Upload PDF</button>
         </div>
     );
 };
